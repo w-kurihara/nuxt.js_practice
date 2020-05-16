e3ac3baf36c0d9c67e16a2d339f1f3f0fce589a2
kurihara <kurihara@iret.co.jp>
2020-05-17 08:50:34 +0900

    modify config

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/w-kurihara/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/nuxt.js_practice/favicon.ico' }
     ]
   },
   /*
@@ -60,6 +60,6 @@ export default {
     }
   },
   router: {
-    base: '/w-kurihara/'
+    base: '/nuxt.js_practice/'
   }
 }
